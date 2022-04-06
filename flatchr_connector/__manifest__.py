5ae00fa0302ed89f6d746a0c3a10d6cc46e57e7a
Elite-s <46265300+Elite-s@users.noreply.github.com>
2022-04-06 14:53:52 +0100

    Update __manifest__.py

--- flatchr_connector/__manifest__.py
+++ flatchr_connector/__manifest__.py
@@ -7,7 +7,7 @@
         """,
     "category": "",
     "version": "15.0.1.0.0",
-    "author": "ELITE Solutions",
+    "author": "ELITE Advanced technologies",
     "website": "http://www.odoo.com",
     "license": "OEEL-1",
     "depends": [
